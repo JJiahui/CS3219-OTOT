47523506668edf2bc0194c3cae15ed7150234d46
JJiahui <jiangjiahui2048@gmail.com>
2020-11-14 17:31:45 +0800

    cleanup

--- frontend/task/vue.config.js
+++ frontend/task/vue.config.js
@@ -1,13 +1,20 @@
 module.exports = {
   devServer: {
-    proxy: 'http://localhost:9090'
-    // proxy: {
-    //   "^/api/": {
-    //     target: "http://localhost:9090",
-    //     pathRewrite: { "^/api/": "/api/" },
-    //     changeOrigin: true,
-    //     logLevel: "debug"
-    //   }
-    // }
+    // proxy: 'http://localhost:9090'
+    proxy: {
+      // "^/api/": {
+      "api/": {
+        target: "http://localhost:9090",
+        // pathRewrite: { "^/api/": "/api/" },
+        // changeOrigin: true,
+        // logLevel: "debug"
+      },
+      "/.netlify/functions": {
+        "target": "http://localhost:9000",
+        "pathRewrite": {
+          "^/\\.netlify/functions": ""
+        }
+      }
+    }
   }
 }
